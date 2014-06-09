b28e82e18271d99a9e513e2cca450debc55fa37a
Cenk Alti <cenkalti@gmail.com>
2014-06-09 11:05:56 +0300

    remove unused fields

--- handler.go
+++ handler.go
@@ -51,16 +51,14 @@ type Formatter interface {
 
 // Record contains all of the information about a single log message.
 type Record struct {
-	format      *string       // Format string
-	args        []interface{} // Arguments to format string
-	Message     string        // Formatted log message
-	LoggerName  string        // Name of the logger module
-	Level       Level         // Level of the record
-	Time        time.Time     // Time of the record (local time)
-	Filename    string        // File name of the log call (absolute path)
-	Line        int           // Line number in file
-	ProcessID   int           // PID
-	ProcessName string        // Name of the process
+	Message     string    // Formatted log message
+	LoggerName  string    // Name of the logger module
+	Level       Level     // Level of the record
+	Time        time.Time // Time of the record (local time)
+	Filename    string    // File name of the log call (absolute path)
+	Line        int       // Line number in file
+	ProcessID   int       // PID
+	ProcessName string    // Name of the process
 }
 
 /////////////////
