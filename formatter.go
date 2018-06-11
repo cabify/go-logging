93a28e6dbc23e7c147ae27ad0a7a72a05f3e7416
Alvaro Polo <alvaro.polo@cabify.com>
2018-06-11 12:45:08 +0200

    Change default formatter to use ms precision in timestamps
    It's really useful to log the things showing the milliseconds of the timestamps. Especially in systems near to baremetal performance.

--- formatter.go
+++ formatter.go
@@ -12,7 +12,7 @@ type defaultFormatter struct{}
 
 // Format outputs a message like "2014-02-28 18:15:57 [example] INFO     something happened"
 func (f defaultFormatter) Format(rec *Record) string {
-	return fmt.Sprintf("%s [%s] %-8s %s", fmt.Sprint(rec.Time)[:19], rec.LoggerName, LevelNames[rec.Level], rec.Message)
+	return fmt.Sprintf("%s [%s] %-8s %s", fmt.Sprint(rec.Time)[:23], rec.LoggerName, LevelNames[rec.Level], rec.Message)
 }
 
 var LevelNames = map[Level]string{
